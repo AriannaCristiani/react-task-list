edf1e264195be1c4f9021cd223138300170154e5
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-26 16:22:01 +0100

    creazione funzione corretta per accedere ai dati dell'array

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -5,11 +5,11 @@ import tasks from './tasks'
 function App() {
 
   const currentTask = tasks.filter((task) => {
-   if(task.state === 'in_progress'){
-    return true
-   }else if(task.state === 'backlog'){
-    return true
-   }
+    if (task.state === 'in_progress') {
+      return true
+    } else if (task.state === 'backlog') {
+      return true
+    }
   })
   //console.log(currentTask)
   const currentTaskCount = currentTask.length
@@ -17,8 +17,8 @@ function App() {
   const completedTask = tasks.filter((task) => task.state === 'completed')
   //console.log(completedTask)
   const completedTaskCount = completedTask.length
-  
-  
+
+
 
   return (
     <>
@@ -27,54 +27,26 @@ function App() {
           <h1><strong>Task Manager</strong></h1>
         </header>
         <main className='main'>
-        <h2>Current Tasks ({currentTaskCount})</h2>
+          <h2>Current Tasks ({currentTaskCount})</h2>
           <ul>
-            <li><strong>{currentTask[0].title} </strong><span className='state'>{currentTask[0].state}</span></li>
-            <li>Priority: {currentTask[0].priority}</li>
-            <li>Est.time: {currentTask[0].estimatedTime} </li>
-
-            <li><strong>{currentTask[1].title}</strong><span className='state'>{currentTask[1].state}</span></li>
-            <li>Priority: {currentTask[1].priority}</li>
-            <li>Est.time: {currentTask[1].estimatedTime} </li>
-
-            <li><strong>{currentTask[2].title}</strong><span className='state'>{currentTask[2].state}</span></li>
-            <li>Priority: {currentTask[2].priority}</li>
-            <li>Est.time: {currentTask[2].estimatedTime} </li>
-
-            <li><strong>{currentTask[3].title}</strong><span className='state'>{currentTask[3].state}</span></li>
-            <li>Priority: {currentTask[3].priority}</li>
-            <li>Est.time: {currentTask[3].estimatedTime} </li>
+            {currentTask.map(element => (
+              <li key={element.id}>
+              <strong>{element.title}</strong><span className='state'>{element.state}</span>
+              <br />
+              Priority: {element.priority}
+              <br />
+              Est.time: {element.estimatedTime} 
+              </li>
+            ))}
           </ul>
 
           <hr />
 
           <h2>Current Tasks ({completedTaskCount})</h2>
           <ul>
-            <li><strong>{completedTask[0].title} </strong><span className='state'>{completedTask[0].state}</span></li>
-            <li>Priority: {completedTask[0].priority}</li>
-            <li>Est.time: {completedTask[0].estimatedTime} </li>
 
-            <li><strong>{completedTask[1].title}</strong><span className='state'>{completedTask[1].state}</span></li>
-            <li>Priority: {completedTask[1].priority}</li>
-            <li>Est.time: {completedTask[1].estimatedTime} </li>
-
-            <li><strong>{completedTask[2].title}</strong><span className='state'>{completedTask[2].state}</span></li>
-            <li>Priority: {completedTask[2].priority}</li>
-            <li>Est.time: {completedTask[2].estimatedTime} </li>
-
-            <li><strong>{completedTask[3].title}</strong><span className='state'>{completedTask[3].state}</span></li>
-            <li>Priority: {completedTask[3].priority}</li>
-            <li>Est.time: {completedTask[3].estimatedTime} </li>
-
-            <li><strong>{completedTask[4].title}</strong><span className='state'>{completedTask[4].state}</span></li>
-            <li>Priority: {completedTask[4].priority}</li>
-            <li>Est.time: {completedTask[4].estimatedTime} </li>
-
-            <li><strong>{completedTask[5].title}</strong><span className='state'>{completedTask[5].state}</span></li>
-            <li>Priority: {completedTask[5].priority}</li>
-            <li>Est.time: {completedTask[5].estimatedTime} </li>
           </ul>
-          
+
         </main>
       </section>
     </>
